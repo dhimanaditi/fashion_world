d27116e92c1d2e84b07659c26d53d2339e2d78de
AditiDhiman <dhimanaditi4@gmail.com>
2022-04-12 14:43:25 +0530

    initial commit

--- script/collection2.js
+++ script/collection2.js
@@ -1,7 +1,5 @@
 /* All functions to be called at loding time are at bottom */
 gdata = ''              //Global products data
-gdata2 = ''              //Global products 2 data
-gdata3 = ''              //Global products 3 data
 cartdata = ''           //Cart data 
 let url = window.localStorage.getItem("url")
 let user = window.sessionStorage.getItem('user')
@@ -14,7 +12,7 @@ function LOAD() {
 
         //Get all products
         $.ajax({
-            url: url + "/products",
+            url: url + "/products2",
             type: "GET",
             success: (posRes) => {
                 gdata = posRes
@@ -48,65 +46,9 @@ function LOAD() {
         
 
     //}
-    $.ajax({
-        url: url + "/products2",
-        type: "GET",
-        success: (posRes) => {
-            gdata2 = posRes
-            let x = '<div class = "row col-12">'
-            for (let i = 0; i < gdata2.length; i++) {
-                x = x + `
-                <div class = 'col-4 '>
-                    <div class = 'card'>
-                        <img src = ${gdata2[i].pic} class = 'card-img-top'>
-                        <div class = 'card-body'>
-                            <div class = "h2 card-title">${gdata2[i].p_name} </div>
-                            <div class = "h4 card-subtitle text-muted">${gdata2[i].p_cost} </div>
-                            <button onclick="learnMore('${i}')" class="btn btn-outline-info btn-block btn-sm">Learn More</button>
-                            <button onclick="addToCart('${i}')" class="btn btn-outline-success btn-block btn-sm">Add to Cart</button>
-                            <button onclick="buyNow('${i}')" class="btn btn-outline-success btn-block btn-sm">Buy Now</button>
-                        </div> 
-                    </div>
-                </div>
-                `
-            }
-            x = x + `</div>`
-            document.getElementById('products2').innerHTML = x
-        },
-        error: (errRes) => {
-            console.log(errRes)
-        }
-    })
+  
     
-    $.ajax({
-        url: url + "/products3",
-        type: "GET",
-        success: (posRes) => {
-            gdata3 = posRes
-            let x = '<div class = "row col-12">'
-            for (let i = 0; i < gdata3.length; i++) {
-                x = x + `
-                <div class = 'col-4 '>
-                    <div class = 'card'>
-                        <img src = ${gdata3[i].pic} class = 'card-img-top'>
-                        <div class = 'card-body'>
-                            <div class = "h2 card-title">${gdata3[i].p_name} </div>
-                            <div class = "h4 card-subtitle text-muted">${gdata3[i].p_cost} </div>
-                            <button onclick="learnMore('${i}')" class="btn btn-outline-info btn-block btn-sm">Learn More</button>
-                            <button onclick="addToCart('${i}')" class="btn btn-outline-success btn-block btn-sm">Add to Cart</button>
-                            <button onclick="buyNow('${i}')" class="btn btn-outline-success btn-block btn-sm">Buy Now</button>
-                        </div> 
-                    </div>
-                </div>
-                `
-            }
-            x = x + `</div>`
-            document.getElementById('products3').innerHTML = x
-        },
-        error: (errRes) => {
-            console.log(errRes)
-        }
-    })
+      
 }
 function learnMore(id) {
     console.log(id)
@@ -192,74 +134,7 @@ function addToCart(id)
         error: (errRes) => {
             console.log(errRes)
         }
-    })    
-    
-    //for products 2
-    $.ajax({
-        url: url + "/cart" + "?q=" + user,  //get all products from cart for loggedin user
-        type: "GET",
-        success: (posRes) => {
-            cartData = posRes
-            for(let i = 0; i < cartData.length; i++)
-            {                
-                if(cartData[i].p_id == gdata2[id].p_id && cartData[i].byed == 0)
-                {                                 
-                    console.log("Comparison success qty = ",cartData[i].qty)      
-                    cqty = 1
-                    ix = i
-                    cartid = cartData[i].id                                                          
-                }                
-            }
-            if(cqty == 1)   //if quanity is 1 product is present, update qty only otherwise make entry in cart
-            {
-                console.log("Present")
-                let data = {}
-                data.uid = user
-                data.p_id = gdata2[id].p_id        
-                data.qty = parseInt(cartData[ix].qty) + 1
-                data.byed = 0
-                $.ajax({            
-                    url : url+"/cart/"+cartid,
-                    type : "PUT",
-                    data : data,
-                    success : (posRes) =>{
-                        console.log(posRes)
-                    },
-                    error : (errRes) =>{
-                        console.log(errRes)
-                    }
-                })
-            }
-            else    
-            {
-                console.log("Absent")
-                let data = {}
-                data.uid = user
-                data.p_id = gdata2[id].p_id        
-                data.qty = 1
-                data.byed = 0                    
-                $.ajax({
-                    url : url+"/cart",
-                    type : "POST",
-                    data : data,
-                    success : (posRes) =>{
-                        console.log(posRes)
-                    },
-                    error : (errRes) =>{
-                        console.log(errRes)
-                    }
-                })
-            }
-            //showCart()
-            
-            
-        },
-        error: (errRes) => {
-            console.log(errRes)
-        }
-    }) 
-    
-   
+    })        
 }
 
 
@@ -306,49 +181,6 @@ function showCart() {
             console.log(errRes)
         }
     })
-
-    //for products 2
-    $.ajax({
-        url: url + "/cart" + "?q=" + user,
-        type: "GET",
-        success: (posRes) => {
-           
-            let cartData = posRes
-            //console.log("Cart data:- ",cartData)
-            let x = '<div>'
-            for (let i = 0; i < cartData.length; i++) {     //iterate cart data
-                if (cartData[i].byed == 0) {    //check for product already purchased or not
-                    for (let j = 0; j < gdata2.length; j++) {    //iterate products data
-                        if (gdata2[j].p_id == cartData[i].p_id) {   //map cart and products with p_id
-                            let obj = gdata[j]
-                            x = x + `
-                                <div class = 'row col-12'>
-                                <div class='col-4'>
-                                    <div class = ' card'>
-                                        <img src = ${obj.pic} class = 'card-img-top'>
-                                        <div class = 'card-body'>
-                                            <div class = "h2 card-title">${obj.p_name} </div>  
-                                            <div class = "h4 text-muted">${cartData[i].qty}</div>                              
-                                            <button onclick="reduceFromCart(${cartData[i].id},${cartData[i].qty},${cartData[i].p_id})" class="btn btn-outline-success btn-block btn-sm">Reduce</button>
-                                        </div> 
-                                    </div>
-                                </div>
-                                </div>
-                                ` 
-                               
-                        }
-                    }
-                }
-            }
-            x = x + `</div>`
-            document.getElementById('cart').innerHTML = x
-           
-        },
-        error: (errRes) => {
-            console.log(errRes)
-        }
-    })
-  
    }
     else{
         document.getElementById('wish').innerHTML = "Unauthorized user " 
